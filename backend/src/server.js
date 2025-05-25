8d3fc4dd406a1335162d4a1dc4db6d054114e9a3
QUAN <miqua2308@gmail.com>
2025-05-25 14:54:18 +0700

    ab

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,8 @@ const app = express();
 const allowedOrigins = [
   process.env.CLIENT_URL || "http://localhost:3000",
   "http://localhost:5173", 
-  "*"
+  "http://*",
+  "https://project-hackathon-cjsh-8x497tjnc-miqua2004s-projects.vercel.app/"
 ];
 
 app.use(
