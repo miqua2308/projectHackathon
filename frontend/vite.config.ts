85a26799e4609fb5d8c35c73f035b7e7d4a4896e
QUAN <miqua2308@gmail.com>
2025-05-25 16:05:14 +0700

    add2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/Lancelot-Solana-Hackathon",
+  base: "/",
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL || 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true
+  }
 });
